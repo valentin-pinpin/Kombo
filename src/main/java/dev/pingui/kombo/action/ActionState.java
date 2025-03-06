5117acf62015f0e4d2094031d1f30ea06e624504
Pinpin <pijnpin@gmail.com>
2025-03-06 19:35:16 +0100

    Add started() method to ActionState

--- src/main/java/dev/pingui/kombo/action/ActionState.java
+++ src/main/java/dev/pingui/kombo/action/ActionState.java
@@ -5,6 +5,10 @@ public enum ActionState {
     STARTED,
     STOPPED;
 
+    public boolean started() {
+        return this == STARTED;
+    }
+
     public static ActionState of(boolean started) {
         return started ? STARTED : STOPPED;
     }
