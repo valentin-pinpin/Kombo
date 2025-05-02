aabd3a3810c34f7955f27279a5c35fe12a412930
Pinpin <pijnpin@gmail.com>
2025-05-02 14:58:05 +0200

    Add timestamp to PlayerInput

--- src/main/java/dev/pingui/kombo/input/PlayerInput.java
+++ src/main/java/dev/pingui/kombo/input/PlayerInput.java
@@ -1,3 +1,8 @@
 package dev.pingui.kombo.input;
 
-public record PlayerInput(InputType type, InputState state) implements Input { }
+public record PlayerInput(InputType type, InputState state, long timestamp) implements Input {
+
+    public PlayerInput(InputType type, InputState state) {
+        this(type, state, System.currentTimeMillis());
+    }
+}
